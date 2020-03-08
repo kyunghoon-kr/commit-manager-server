7663716196a271271c2a0c647c29a38c596ab0d5
kimkyunghoon <byul2sol@naver.com>
2020-03-08 14:12:07 +0900

    user api 추가 1. 사용자가 존재하는지 안하는지 여부를 반환하여 response에 넣어 보내기 2. data.name이 존재하는지 안하는지로 판단

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const commitRouter = require('./routes/commit');
 const usernameRouter = require('./routes/username');
+const userRouter = require('./routes/user');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -8,6 +9,7 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
 app.use('/', commitRouter);
 app.use('/', usernameRouter);
+app.use('/', userRouter);
 app.listen(app.get('port'), () => { 
     console.log('익스프레스 서버를 시작합니다, Port : ' + app.get('port'));
 });
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const api = require('./api')
 const router = express.Router();
-router.get('/username', async (req, res) => {
-    console.log('/username 처리 라우팅')
-    
+router.get('/user', async (req, res) => {
+    console.log('/user 처리 라우팅')
     const paramId = req.query.id;
     const paramToken = req.query.token;
     const data = await api.fetchUserInfo(paramId, paramToken);
-    res.send(data.name);
+    if(data.name) {
+        res.send(true);
+    } else res.send(false);
 });
 // 미들웨어에서 파라미터 확인
 
