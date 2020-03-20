604569ce669e3387edb6ffa5df30b9e434e3e6f9
kimkyunghoon <byul2sol@naver.com>
2020-03-20 18:02:44 +0900

    포트 변경

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const usernameRouter = require('./routes/username');
 const userRouter = require('./routes/user');
 const app = express();
 
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 3001);
 
 // app.use(express.static('public'));
 app.use('/', commitRouter);
