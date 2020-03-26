d884f8e452565524e20039184f00666613746e3f
kimkyunghoon <byul2sol@naver.com>
2020-03-26 10:54:50 +0900

    Api 변경에 따른 요청 Url 변경

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,9 +2,8 @@ const axios = require('axios');
 const fetchEvents = async (username, token) => {
     // 토큰을 헤더에 달기
     console.log('fetchEvents 함수 호출');
-    const tokenHeader = `token ${token}`
     const config = {
-        headers: { "Authorization": tokenHeader }
+        headers: { "Authorization": token }
     }
     try {
         const response = await axios.get(`https://api.github.com/users/${username}/events`, config);
